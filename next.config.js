9d5e0949e6890cf0f48963001d62e91c099628d7
PriyaGhimiray18 <02230143.cst@rub.edu.bt>
2025-05-23 19:29:32 +0600

    fix: simplify PostCSS configuration and update dependencies

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
   },
   // Disable font optimization temporarily
   optimizeFonts: false,
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 }
 
 module.exports = nextConfig 
