3e4020979990806145acaa08ec735bb31c062190
PriyaGhimiray18 <02230143.cst@rub.edu.bt>
2025-05-23 18:01:41 +0600

    chore: remove loading and not-found pages

--- src/app/not-found.js
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="container my-5 text-center">
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <a href="/" className="btn btn-primary">Go Home</a>
-    </div>
-  );
-} 
